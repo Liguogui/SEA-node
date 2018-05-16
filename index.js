9b6b00bf6fdbb7695f69668a7a43e679b8e1eca7
Liguogui <tony@gmail.com>
2018-05-16 19:27:09 +0800

    lsjdlsjfsalj

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ var express = require('express')
 var app = express()
 
 // /static/css/index2.css
-app.get('/static/css/app.4d43240d208881469ffc0249d64c1e57.css',function (req,res) {
-    res.sendFile(__dirname+'/static/css/app.4d43240d208881469ffc0249d64c1e57.css')
+app.get('/static/css/app.1cfde8937d2164607a8298220f775d88.css',function (req,res) {
+    res.sendFile(__dirname+'/static/css/app.1cfde8937d2164607a8298220f775d88.css')
 })
 //js
-app.get('/static/js/manifest.9a29538943d72bf6b6c5.js',function (req,res) {
-    res.sendFile(__dirname+'/static/js/manifest.9a29538943d72bf6b6c5.js')
+app.get('/static/js/manifest.07f010b62ab8fbf94acc.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/manifest.07f010b62ab8fbf94acc.js')
 })
-app.get('/static/js/vendor.6a8923c78fa5e80e3cdf.js',function (req,res) {
-    res.sendFile(__dirname+'/static/js/vendor.6a8923c78fa5e80e3cdf.js')
+app.get('/static/js/vendor.e45463390e8519f52d89.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/vendor.e45463390e8519f52d89.js')
 })
-app.get('/static/js/app.17f4e148d2b4a25171ab.js',function (req,res) {
-    res.sendFile(__dirname+'/static/js/app.17f4e148d2b4a25171ab.js')
+app.get('/static/js/app.73383a5523c3980f283b.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/app.73383a5523c3980f283b.js')
 })
+//js组件爆炸
+
+
 
 app.get('/',function (req,res) {
-    res.sendFile(__dirname+'/index2.html')
+    res.sendFile(__dirname+'/index.html')
 })
 
 
