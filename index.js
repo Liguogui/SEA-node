0bc7350ec88b69aad641106a068206860bd951d5
Liguogui <tony@gmail.com>
2018-05-16 20:49:14 +0800

    ai

--- index.js
+++ index.js
@@ -21,81 +21,81 @@ app.get('/static/js/app.73383a5523c3980f283b.js',function (req,res) {
 //api
 // var apiRoutes = express.Router()
 //获取热门歌单
-app.get('/api/getDiscList',function (req,res) {
-    var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg';
-    axios.get(url,{
-        headers:{
-            referer:'https://c.y.qq.com/',
-            host:'c.y.qq.com'
-        },
-        params:req.query
-    }).then((response)=>{
-        res.json(response.data);
-    }).catch((e) =>{
-        console.log(e);
-    })
-});
+// app.get('/api/getDiscList',function (req,res) {
+//     var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg';
+//     axios.get(url,{
+//         headers:{
+//             referer:'https://c.y.qq.com/',
+//             host:'c.y.qq.com'
+//         },
+//         params:req.query
+//     }).then((response)=>{
+//         res.json(response.data);
+//     }).catch((e) =>{
+//         console.log(e);
+//     })
+// });
 //获取歌曲url
-app.post('/api/getPurlUrl', bodyParser.json(), function (req, res) {
-    const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
-    axios.post(url, req.body, {
-        headers: {
-            referer: 'https://y.qq.com/',
-            origin: 'https://y.qq.com',
-            'Content-type': 'application/x-www-form-urlencoded'
-        }
-    }).then((response) => {
-        res.json(response.data)
-    }).catch((e) => {
-        console.log(e)
-    })
-})
-app.get('/api/lyric', function (req, res) {
-    const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
-
-    axios.get(url, {
-        headers: {
-            referer: 'https://c.y.qq.com/',
-            host: 'c.y.qq.com'
-        },
-        params: req.query
-    }).then((response) => {
-        let ret = response.data
-        if (typeof ret === 'string') {
-            const reg = /^\w+\(({.+})\)$/
-            const matches = ret.match(reg)
-            if (matches) {
-                ret = JSON.parse(matches[1])
-            }
-        }
-        res.json(ret)
-    }).catch((e) => {
-        console.log(e)
-    })
-})
+// app.post('/api/getPurlUrl', bodyParser.json(), function (req, res) {
+//     const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+//     axios.post(url, req.body, {
+//         headers: {
+//             referer: 'https://y.qq.com/',
+//             origin: 'https://y.qq.com',
+//             'Content-type': 'application/x-www-form-urlencoded'
+//         }
+//     }).then((response) => {
+//         res.json(response.data)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// })
+// app.get('/api/lyric', function (req, res) {
+//     const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+//
+//     axios.get(url, {
+//         headers: {
+//             referer: 'https://c.y.qq.com/',
+//             host: 'c.y.qq.com'
+//         },
+//         params: req.query
+//     }).then((response) => {
+//         let ret = response.data
+//         if (typeof ret === 'string') {
+//             const reg = /^\w+\(({.+})\)$/
+//             const matches = ret.match(reg)
+//             if (matches) {
+//                 ret = JSON.parse(matches[1])
+//             }
+//         }
+//         res.json(ret)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// })
 //获取歌单的对应音乐
-app.get('/api/getCdInfo', function (req, res) {
-    const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
-    axios.get(url, {
-        headers: {
-            referer: 'https://c.y.qq.com/',
-            host: 'c.y.qq.com'
-        },
-        params: req.query
-    }).then((response) => {
-        let ret = response.data
-        if (typeof ret === 'string') {
-            const reg = /^\w+\(({.+})\)$/
-            const matches = ret.match(reg)
-            if (matches) {
-                ret = JSON.parse(matches[1])
-            }
-        }
-        res.json(ret)
-    }).catch((e) => {
-        console.log(e)
-    })
-})
+// app.get('/api/getCdInfo', function (req, res) {
+//     const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+//     axios.get(url, {
+//         headers: {
+//             referer: 'https://c.y.qq.com/',
+//             host: 'c.y.qq.com'
+//         },
+//         params: req.query
+//     }).then((response) => {
+//         let ret = response.data
+//         if (typeof ret === 'string') {
+//             const reg = /^\w+\(({.+})\)$/
+//             const matches = ret.match(reg)
+//             if (matches) {
+//                 ret = JSON.parse(matches[1])
+//             }
+//         }
+//         res.json(ret)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// })
 
 // app.use('/api', apiRoutes);
 
