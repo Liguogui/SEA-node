ce4c732e0cb47fc09cfe6e5fd080420e433f5d70
Liguogui <tony@gmail.com>
2018-05-16 19:06:07 +0800

    sifjsldjflsk

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var express = require('express')
 var app = express()
 
-app.get('/',function (req,res) {
-    res.sendFile(__dirname+'/index2.html')
+// /static/css/index2.css
+app.get('/static/css/app.4d43240d208881469ffc0249d64c1e57.css',function (req,res) {
+    res.sendFile(__dirname+'/static/css/app.4d43240d208881469ffc0249d64c1e57.css')
+})
+//js
+app.get('/static/js/manifest.9a29538943d72bf6b6c5.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/manifest.9a29538943d72bf6b6c5.js')
+})
+app.get('/static/js/vendor.6a8923c78fa5e80e3cdf.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/vendor.6a8923c78fa5e80e3cdf.js')
+})
+app.get('/static/js/app.17f4e148d2b4a25171ab.js',function (req,res) {
+    res.sendFile(__dirname+'/static/js/app.17f4e148d2b4a25171ab.js')
 })
 
-// /static/css/index2.css
-app.get('/static/css/index2.css',function (req,res) {
-    res.sendFile(__dirname+'/static/css/index2.css')
+app.get('/',function (req,res) {
+    res.sendFile(__dirname+'/index2.html')
 })
 
 
