ce9f37e17e814158112375f736aee3de7c281a66
Liguogui <tony@gmail.com>
2018-05-16 18:30:20 +0800

    skl

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/',function (req,res) {
+    res.sendFile(__dirname+'/index.html')
 })
 
-app.listen(process.env.PORT || 5050)
+var port = process.env.PORT || 5050;
+
+//app.listen(process.env.PORT || 5050)
+
+module.exports = app.listen(port,function (err) {
+    if(err){
+        console.log(err);
+    }
+    console.log('Listening.....');
+});
